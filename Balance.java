1465700c677c2f730193f5f02d022bc35c6e30de
Sahib Singh <51374188+SahibSingh1311@users.noreply.github.com>
2022-09-07 21:17:05 +0530

    Add files via upload

---
+++ Balance.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Balance{
+public static void main(String[] args){
+	String s1 = "{{{{";
+	System.out.println(balancePara(s1));
+}
+static String balancePara(String inp){
+	HashMap<Character,Integer> map = new HashMap<>();
+	int left = 0, right=0;
+	for(int i=0;i<inp.length();i++){
+		if(map.containsKey(inp.charAt(i))){
+			map.put(inp.charAt(i),map.get(inp.charAt(i))+1);
+		}else{
+			map.put(inp.charAt(i),1);		
+		}
+	}
+	left = map.get('{')==null?0:map.get('{');
+	right= map.get('}')==null?0:map.get('}');
+	while(left!=right){
+	if(left<right){
+		inp = "{"+inp;
+		left++; 
+		}
+	if(right<left){
+		inp +="}";
+		right++;
+		}	
+	}
+	return inp;
+}
+}
